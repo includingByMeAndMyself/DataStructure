e321cdd0011ee2fe0f70fedd89886dfa1ad53fb0
includingivansmirnov@gmail.com <includingivansmirnov@gmail.com>
2022-05-26 13:30:39 +0500

    Реализация Двухсвязного списка

---
+++ DataStructure.Lib/Lists/Duplexitem.cs
@@ -0,0 +1,22 @@
+﻿
+namespace DataStructure.Lib.Lists
+{
+    public class Duplexitem <T>
+    {
+        public T Data { get; set; }
+
+        public Duplexitem<T> Previous { get; set; }
+
+        public Duplexitem<T> Next { get; set; }
+
+        public Duplexitem(T data)
+        {
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
